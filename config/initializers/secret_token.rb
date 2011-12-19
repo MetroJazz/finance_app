c9f06e9974e380df6b5338e25af3f4a3165e21b5
Eduard Milevsky <eduardmilevsky@gmail.com>
2011-12-19 12:13:16 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinanceApp::Application.config.secret_token = '5ca4a01f7c4c62b48f56fcce23c850b69581b4f1340fd8fa83af51dc02d7a73ceb1ba2628250da3ea7ff792968fd36ca134a8908ced4463decc90a3904f4a9be'
